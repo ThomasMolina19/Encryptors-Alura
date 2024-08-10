f0bdea4d2a535ddc5da351f1939a01ee469ad6e8
ThomasMolina <thomasmolina428@gmail.com>
2024-08-09 21:37:55 -0500

    Le agregue el efecto de cascada de matrix

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var p1 = 167;
 var p2 = 149;
 var n = p1 * p2;
 var phyn = (p1 - 1) * (p2 - 1);
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var W = window.innerWidth;
+var H = window.innerHeight;
+var fontSize = 16;
+var columns = Math.floor(W / fontSize);
+var drops = [];
+var rainInterval;
+var isRaining = true;
 var abecedario = [
   "a",
   "b",
@@ -32,6 +41,7 @@ var abecedario = [
   "z",
   " ",
 ];
+
 
 // halla el maximo comun divisor entre phy(n) y e, para poder encontrar el e apropiado que de como resultado gcd(e,phy(n))=1
 function mcd(a, b) {
@@ -220,6 +230,45 @@ function desencriptar() {
   muestra.value = arrayString;
 }
 
+function encriptacionalura() {
+
+  const text = document.getElementById("criptacion");
+  const muestra = document.getElementById("muestra");
+  const texto = text.value;
+
+  // Mapeo de letras a sus reemplazos
+  const reemplazos = {
+      'a': 'ai',
+      'e': 'enter',
+      'i': 'imes',
+      'o': 'ober',
+      'u': 'ufat'
+  };
+
+  // Reemplazar las letras usando replace y una función
+  let nuevoTexto = texto.replace(/[aeiou]/g, letra => reemplazos[letra]);
+
+  // junta los numeros y los mete en una lista
+  muestra.value = nuevoTexto;
+}
+
+function desencriptacionAlura() {
+  const muestra = document.getElementById("muestra");
+  const text = document.getElementById("criptacion");
+  const texto = text.value;
+  
+  //Reemplazar las letras usando replace 
+  const nuevoTexto = texto
+  .replace(/enter/g, 'e')
+  .replace(/imes/g, 'i')
+  .replace(/ai/g, 'a')
+  .replace(/ober/g, 'o')
+  .replace(/ufat/g, 'u');
+
+//envia el nuevo texto
+muestra.value = nuevoTexto;
+}
+
 function copiar(){
 
   //utiliza la api clipboard.api
@@ -232,11 +281,60 @@ function copiar(){
 function pegar(){
   
   navigator.clipboard.readText().then(function(clipText) {
-    
+
     document.getElementById("criptacion").value = clipText;
     }, function() {
     alert("Failed to access clipboard.");
+    
     })
 }
+function draw() {
+  context.fillStyle = "rgba(0,0,0,0.05)";
+  context.fillRect(0, 0, W, H);
+  context.font = "700 " + fontSize + "px";
+  context.fillStyle = "#00cc33";
+
+  for (var i = 0; i < columns; i++) {
+      var index = Math.floor(Math.random() * str.length);
+      var x = i * fontSize;
+      var y = drops[i] * fontSize;
+      context.fillText(str[index], x, y);
+
+      if (y >= canvas.height && Math.random() > 0.99) {
+          drops[i] = 0;
+      }
+
+      drops[i]++;
+  }
+}
+
+function startRain() {
+  rainInterval = setInterval(draw, 35);
+}
+
+function stopRain() {
+  clearInterval(rainInterval);
+}
+
+function toggleRain() {
+  if (isRaining) {
+      stopRain();
+  } else {
+      startRain();
+  }
+  isRaining = !isRaining;
+}
+
+
+canvas.width = W;
+canvas.height = H;
+for (var i = 0; i < columns; i++) {
+  drops.push(0);
+}
+
+var str = "JavaScript Hacking Effect";
+
+startRain();
+
 
 
